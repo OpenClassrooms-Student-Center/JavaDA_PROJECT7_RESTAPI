4b137f897ba7bd85b91c64bf507c6a9f8802d26d
Stevens <29457062+xxStevensxx@users.noreply.github.com>
2021-09-28 22:39:54 +0200

    Ajout de @ComponentScan(basePackages = {com.nnk.springboot.repositories})

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/Application.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/Application.java
@@ -2,8 +2,11 @@ package com.nnk.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@ComponentScan(basePackages = {"com.nnk.springboot.repositories"})
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class Application {
 
 	public static void main(String[] args) {
