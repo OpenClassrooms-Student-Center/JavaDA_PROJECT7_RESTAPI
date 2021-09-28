27b2749e2ca46f51940c216b0f6ac5889e36f422
Stevens <29457062+xxStevensxx@users.noreply.github.com>
2021-09-29 00:31:59 +0200

    Ajout des dependances swagger dans le pom.Xml ajout de l'annotation @ pour swagger dans le main creation ddu package et de la config swagger ajout du banner de l'app

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/Application.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/Application.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
 @ComponentScan(basePackages = {"com.nnk.springboot.repositories"})
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class Application {
