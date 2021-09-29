ffdaf214b0553e80697806f16fc0847143d200f2
xxStevensxx <29457062+xxStevensxx@users.noreply.github.com>
2021-09-29 12:29:59 +0200

    Maj de Spring config

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/security/SecurityConfig.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/security/SecurityConfig.java
@@ -4,11 +4,16 @@ import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Autowired
@@ -27,10 +32,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		
 		http.authorizeRequests()
-			.antMatchers("/", "/add", "/list", "/update").authenticated()
+			.antMatchers("/bidList/**", "/curvePoint/**", "/rating/**", "/ruleName/**", "/trade/**").hasAnyAuthority("USER")
+			.antMatchers("/user/**").permitAll()
 			.and()
 			.formLogin()
-			.loginPage("/home")
+			.defaultSuccessUrl("/bidList/list")
 			.and()
 			.logout()
 			.logoutUrl("/logout")
@@ -40,10 +46,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	}
 		
 	
-//	@Bean
-//	public PasswordEncoder passwordEncoder() {
-//		return new BCryptPasswordEncoder();
-//
-//	}
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+
+	}
 
 }
