f881360409a5df7cbfcade7ffef949300b2fda15
Fabien De deken <fabien.dedeken@yahoo.fr>
2022-02-28 12:43:18 +0100

    implements code to pass tests from UserTests

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/User.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/User.java
@@ -64,4 +64,28 @@ public class User {
     public void setRole(String role) {
         this.role = role;
     }
+
+	public User(
+			@NotBlank(message = "Username is mandatory") @Length(max = 125, message = "must have 125 caracters max") String username,
+			@NotBlank(message = "Password is mandatory") @Length(max = 125, message = "must have 125 caracters max") String password,
+			@NotBlank(message = "FullName is mandatory") @Length(max = 125, message = "must have 125 caracters max") String fullname,
+			@NotBlank(message = "Role is mandatory") @Length(max = 125, message = "must have 125 caracters max") String role) {
+		super();
+		this.username = username;
+		this.password = password;
+		this.fullname = fullname;
+		this.role = role;
+	}
+	
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String toString() {
+		return "User [username=" + username + ", password=" + password + ", fullname=" + fullname + ", role=" + role
+				+ "]";
+	}
+    
 }
