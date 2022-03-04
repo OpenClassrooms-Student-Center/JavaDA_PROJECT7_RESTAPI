95e16081e4281a21aaa5037a10d0e6bf0bf5b08e
Fabien De deken <fabien.dedeken@yahoo.fr>
2022-03-04 10:08:42 +0100

    implements front for Rating entity

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/Rating.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/Rating.java
@@ -1,31 +1,33 @@
 package com.nnk.springboot.domain;
 
 import javax.persistence.*;
+
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import java.sql.Timestamp;
 
 @Entity
 @Table(name = "Rating")
 public class Rating {
-    // TODO: Map columns in data table RATING with corresponding java fields
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@NotNull
 	@Digits(integer = 4, fraction = 0)
 	@Column(name = "Id")
 	private Integer id;
 	
+	@NotBlank(message = "MoodysRating is mandatory")
 	@Column(name = "moodysRating")
 	private String moodysRating;
 	
+	@NotBlank(message = "SandRating is mandatory")
 	@Column(name = "sandPRating")
 	private String sandPRating;
 	
+	@NotBlank(message = "FitchRating is mandatory")
 	@Column(name = "fitchRating")
 	private String fitchRating;
 	
+	@NotNull(message = "must not be null")
 	@Column(name = "orderNumber")
 	private Integer orderNumber;
 
