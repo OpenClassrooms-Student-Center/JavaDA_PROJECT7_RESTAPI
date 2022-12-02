9e85805015a22c6c8375cd9ea5c3b6077c927f90
tylau-dev <lautsyyeung@gmail.com>
2022-12-02 17:55:32 +0100

    validator + forms for bidlist and trade

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/BidListController.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/BidListController.java
@@ -14,83 +14,82 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.nnk.springboot.domain.BidList;
-import com.nnk.springboot.domain.CurvePoint;
-import com.nnk.springboot.repositories.BidListRepository;
 import com.nnk.springboot.services.IBidListService;
+import com.nnk.springboot.validator.BidValidator;
 
 @Controller
 public class BidListController {
-	private IBidListService bidListService;
+    private IBidListService bidListService;
+    private BidValidator bidValidator;
 
-    public BidListController(IBidListService bidListService) {
-    	this.bidListService = bidListService;
+    public BidListController(IBidListService bidListService, BidValidator bidValidator) {
+	this.bidListService = bidListService;
+	this.bidValidator = bidValidator;
     }
-	
+
     @RequestMapping("/bidList/list")
     public String home(Model model) {
-    	
-    	List<BidList> bidListResult = new ArrayList<BidList>();
-    	this.bidListService.getBidLists().forEach(bidListResult::add);
-    	model.addAttribute("bidList", bidListResult);
 
-    	return "bidList/list";
+	List<BidList> bidListResult = new ArrayList<BidList>();
+	this.bidListService.getBidLists().forEach(bidListResult::add);
+	model.addAttribute("bidList", bidListResult);
+
+	return "bidList/list";
     }
 
     @GetMapping("/bidList/add")
     public String addBidForm(BidList bid) {
-    	return "bidList/add";
+	return "bidList/add";
     }
 
     @PostMapping("/bidList/validate")
     public String validate(@Valid BidList bid, BindingResult result, Model model) {
-    	// @todo bidValidator
-//    	bidValidator.validate(bid, result);
-//    	if (result.hasErrors()) {
-//    	    return "bidList/add";
-//    	}
-    	
-    	this.bidListService.saveBidList(bid);
-	
-    	return "bidList/add";
+	bidValidator.validate(bid, result);
+	if (result.hasErrors()) {
+	    return "bidList/add";
+	}
+
+	this.bidListService.saveBidList(bid);
+
+	return "bidList/add";
     }
 
     @GetMapping("/bidList/update/{id}")
     public String showUpdateForm(@PathVariable("id") Integer id, Model model) {
-    	BidList bidListResult = this.bidListService.getBidListById(id).get();
-    	
-    	model.addAttribute("bidListId", bidListResult);
-    	
-    	return "bidList/update";
+	BidList bidListResult = this.bidListService.getBidListById(id).get();
+
+	model.addAttribute("bidList", bidListResult);
+
+	return "bidList/update";
     }
 
     @PostMapping("/bidList/update/{id}")
     public String updateBid(@PathVariable("id") Integer id, @Valid BidList bidList, BindingResult result, Model model) {
-    	// @todo bidValidator
-//    	bidValidator.validate(bidList, result);
-//    	if (result.hasErrors()) {
-//    	    return $"bidList/update/{id}";
-//    	}
-    	
-    	// Save also updates automatically with Entity Framework
-    	this.bidListService.saveBidList(bidList);
+	bidValidator.validate(bidList, result);
+	if (result.hasErrors()) {
+	    return "bidList/update";
+	}
 
-    	// Retrieve the BidList with updated values
-    	List<BidList> bidListResult = new ArrayList<BidList>();
-    	this.bidListService.getBidLists().forEach(bidListResult::add);
-    	model.addAttribute("bidList", bidListResult);
-    	
+	// Save also updates automatically with Entity Framework
+	this.bidListService.saveBidList(bidList);
+
+	// Retrieve the BidList with updated values
+	List<BidList> bidListResult = new ArrayList<BidList>();
+	this.bidListService.getBidLists().forEach(bidListResult::add);
+	model.addAttribute("bidList", bidListResult);
+
 	return "redirect:/bidList/list";
     }
 
     @GetMapping("/bidList/delete/{id}")
     public String deleteBid(@PathVariable("id") Integer id, Model model) {
-    	this.bidListService.deleteBidListById(id);
-    	
-    	// Retrieve the BidList with updated values
-    	List<BidList> bidListResult = new ArrayList<BidList>();
-    	this.bidListService.getBidLists().forEach(bidListResult::add);
-    	model.addAttribute("bidList", bidListResult);
+	this.bidListService.deleteBidListById(id);
 
-    	return "redirect:/bidList/list";
+	// Retrieve the BidList with updated values
+	List<BidList> bidListResult = new ArrayList<BidList>();
+	this.bidListService.getBidLists().forEach(bidListResult::add);
+	model.addAttribute("bidList", bidListResult);
+
+	return "redirect:/bidList/list";
     }
 }
