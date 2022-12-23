dbca4c131efa2095f92696c41db49c5f3b9e6118
Daniele410 <miraglia.daniele410@gmail.com>
2022-12-23 11:06:55 +0100

    fix login

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/security/SecurityConfig.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/security/SecurityConfig.java
@@ -47,6 +47,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.csrf().disable();
         http.authorizeRequests().antMatchers(
                         "/registration/**",
+                        "user/**",
                         "/js/**",
                         "/css/**",
                         "/img/**").permitAll()
@@ -68,53 +69,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 
     }
-
-//    @Override
-//    public void configure(WebSecurity web) {
-//        web.ignoring()
-//                .antMatchers("/css/**", "/static/**", "/img/**")
-//                .antMatchers("/h2-console/**");
-//
-//    }
-
-
-//    private UserPrincipal userPrincipal;
-//
-//    @Bean
-//    public PasswordEncoder passwordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
-//
-//    @Override
-//        protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//
-////        auth.userDetailsService(userDetailsService()).passwordEncoder(passwordEncoder());
-//
-//            auth.inMemoryAuthentication()
-//                    .withUser("springUser").password(passwordEncoder().encode("spring123")).roles("USER")
-//                    .and()
-//                    .withUser("springAdmin").password(passwordEncoder().encode("admin123")).roles("ADMIN", "USER");
-//
-////            auth.jdbcAuthentication().withUser(userPrincipal.getUsername()).password(passwordEncoder().encode(userPrincipal.getPassword())).roles("USER")
-////                    .and()
-////                    .withUser(userPrincipal.getUsername()).password(passwordEncoder().encode(userPrincipal.getPassword())).roles("ADMIN", "USER");
-//
-//
-//        }
-
-//        @Override
-//        public void configure(HttpSecurity http) throws Exception {
-//            http.csrf()
-//                    .disable()
-//                    .authorizeRequests()
-//                    .antMatchers("/home", "/", "/403").permitAll()
-//                    .antMatchers("/user/**").hasAuthority("ADMIN")
-//                    .anyRequest().authenticated()
-//                    .and()
-//                    .formLogin()
-//                    .and()
-//                    .oauth2Login();
-//        }
 
 
 }
