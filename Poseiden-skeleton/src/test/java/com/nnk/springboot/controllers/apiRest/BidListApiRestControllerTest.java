e305c29cbf8047cba818ef3c1e328f8144da95f5
Daniele410 <miraglia.daniele410@gmail.com>
2023-01-03 13:06:01 +0100

    add integrationTestBid

--- Poseiden-skeleton/src/test/java/com/nnk/springboot/controllers/apiRest/BidListApiRestControllerTest.java
+++ Poseiden-skeleton/src/test/java/com/nnk/springboot/controllers/apiRest/BidListApiRestControllerTest.java
@@ -1,11 +1,12 @@
 package com.nnk.springboot.controllers.apiRest;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nimbusds.jose.shaded.json.JSONObject;
 import com.nnk.springboot.domain.BidList;
 import com.nnk.springboot.domain.User;
+import com.nnk.springboot.exception.DataNotFoundException;
 import com.nnk.springboot.repositories.BidListRepository;
-import org.json.JSONException;
-import org.json.JSONObject;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,11 +17,15 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
+import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 @ActiveProfiles("test")
 //@WebMvcTest(controllers = BidListApiRestController.class)
 @AutoConfigureMockMvc
@@ -38,13 +43,10 @@ class BidListApiRestControllerTest {
     private User user1;
 
 
-
-
-
     @BeforeEach
     void setup() {
         new ObjectMapper();
-        user1 = new User("Jimmy", "Jimmy", "12345","ADMIN");
+        user1 = new User("Jimmy", "Jimmy", "12345", "ADMIN");
 
     }
 
@@ -53,30 +55,50 @@ class BidListApiRestControllerTest {
     // When
     // Then
 
-//    @Test
-//    void showRestBid() throws Exception {
-//        String connectedUser = SecurityContextHolder.getContext().getAuthentication().getName();
-//
-//        //        UserDetails user = User.builder()
-////                .username("user")
-////                .password("$2a$12$uCNqI/ReIfRsZ/A/dfTl9O1LQCfiZhJ7AHctjBOQWdR9xSspaMa6m")
-////                .roles("USER")
-////                .build();
-//        GrantedAuthority authority = new SimpleGrantedAuthority(user1.getRole());
-//        when(userDetailService.loadUserByUsername(any()))
-//                .thenReturn((UserDetails) new User(user1.getUsername(), user1.getPassword(), String.valueOf(Collections.singletonList(authority))));
-//
-//        when(bidListService.findAll()).thenReturn(any());
-//
-//        mockMvc.perform(MockMvcRequestBuilders.get("/bidList/api")
-//                .contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk());
-//
-//
-//    }
+    @Test
+    void showRestBid() throws Exception {
+        //GIVEN
+        JSONObject json = new JSONObject();
+        json.put("account", "toto");
+        json.put("type", "tutu");
+        json.put("bidQuantity", 20);
+
+        //WHEN
+        mockMvc.perform(MockMvcRequestBuilders.post("/bidList/api")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json.toString()))
+                .andExpect(status().isOk());
+
+        mockMvc.perform(get("/bidList/api"))
+                .andExpect(status().isOk());
+        //THEN
+        List<BidList> result = bidListRepository.findAll();
+        assertEquals("toto", result.get(0).getAccount());
+
+
+    }
 
     @Test
-    void showRestBidById() {
+    void showRestBidById() throws Exception {
+        //GIVEN
+        JSONObject json = new JSONObject();
+        json.put("account", "toto");
+        json.put("type", "tutu");
+        json.put("bidQuantity", 20);
+
+        //WHEN
+        mockMvc.perform(MockMvcRequestBuilders.post("/bidList/api")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json.toString()))
+                .andExpect(status().isOk());
+
+        mockMvc.perform(get("/bidList/api/{id}", 1L))
+                .andExpect(status().isOk());
+        //THEN
+        Optional<BidList> result = bidListRepository.findById(1);
+        assertEquals("toto", result.get().getAccount());
     }
+
 
     @Test
     void addRestBid() throws Exception {
@@ -88,8 +110,8 @@ class BidListApiRestControllerTest {
 
         //WHEN
         mockMvc.perform(MockMvcRequestBuilders.post("/bidList/api")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(json.toString()))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json.toString()))
                 .andExpect(status().isOk());
         //THEN
         List<BidList> result = bidListRepository.findAll();
@@ -97,10 +119,59 @@ class BidListApiRestControllerTest {
     }
 
     @Test
-    void uploadRestBid() {
+    void uploadRestBid() throws Exception {
+        //GIVEN
+        JSONObject json = new JSONObject();
+        json.put("bidListId", 1);
+        json.put("account", "toto");
+        json.put("type", "tutu");
+        json.put("bidQuantity", 20);
+
+        //WHEN
+        mockMvc.perform(MockMvcRequestBuilders.post("/bidList/api")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json.toString()))
+                .andExpect(status().isOk());
+
+        JSONObject json1 = new JSONObject();
+        json1.put("bidListId", 1);
+        json1.put("account", "Sa,");
+        json1.put("type", "tutu");
+        json1.put("bidQuantity", 18);
+
+        mockMvc.perform(MockMvcRequestBuilders.put("/bidList/api")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json1.toString()))
+                .andExpect(status().isOk());
+        //THEN
+        List<BidList> result = bidListRepository.findAll();
+        assertEquals(1, result.size());
+
+
     }
 
     @Test
-    void deleteRestBid() {
+    void deleteRestBid() throws Exception {
+
+        //GIVEN
+        JSONObject json = new JSONObject();
+        json.put("bidListId", 1);
+        json.put("account", "toto");
+        json.put("type", "tutu");
+        json.put("bidQuantity", 20);
+
+        //WHEN
+        mockMvc.perform(MockMvcRequestBuilders.post("/bidList/api")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json.toString()))
+                .andExpect(status().isOk());
+
+
+        mockMvc.perform(MockMvcRequestBuilders.delete("/bidList/api/{id}", 1L)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+        //THEN
+        List<BidList> result = bidListRepository.findAll();
+        assertEquals(0, result.size());
     }
 }
