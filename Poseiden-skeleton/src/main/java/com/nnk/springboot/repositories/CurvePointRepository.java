fa5ef8e45cf4fecd4053f889a279e18b6a1936be
abel <kisebela.a@gmail.com>
2023-04-08 23:20:54 +0200

    POS-3 : Ajout de @Repository sur tous les fichiers du package repositories

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/repositories/CurvePointRepository.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/repositories/CurvePointRepository.java
@@ -2,8 +2,9 @@ package com.nnk.springboot.repositories;
 
 import com.nnk.springboot.domain.CurvePoint;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-
+@Repository
 public interface CurvePointRepository extends JpaRepository<CurvePoint, Integer> {
 
 }
--- Poseiden-skeleton/src/main/java/com/nnk/springboot/repositories/RatingRepository.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/repositories/RatingRepository.java
@@ -2,7 +2,9 @@ package com.nnk.springboot.repositories;
 
 import com.nnk.springboot.domain.Rating;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RatingRepository extends JpaRepository<Rating, Integer> {
 
 }
