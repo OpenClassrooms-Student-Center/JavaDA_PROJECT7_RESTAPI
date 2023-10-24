2fb2a4a1fbe40e0440b912fd8f47c6f7f0d7e0e9
SimonArduin <125870016+SimonArduin@users.noreply.github.com>
2023-10-24 17:35:14 +0200

    added basic integration test for Rating

--- Poseiden-skeleton/src/test/java/com/nnk/springboot/integration/RatingIntegrationTests.java
+++ Poseiden-skeleton/src/test/java/com/nnk/springboot/integration/RatingIntegrationTests.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -29,6 +30,7 @@ public class RatingIntegrationTests extends TestVariables {
     public void contextLoads() {}
 
     @Test
+    @WithMockUser
     public void homeTest() throws Exception {
         mockMvc.perform(get("/rating/list")).andExpect(status().is2xxSuccessful());
     }
