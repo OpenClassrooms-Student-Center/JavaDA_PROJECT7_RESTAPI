53f434f8f2e7d9271cf83e03abb9d79e26d4c892
Mikael Heyraud <mikaelhey@hotmail.fr>
2023-11-02 19:14:00 +0100

    MHE - create bid repository Methods

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/repositories/BidListRepository.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/repositories/BidListRepository.java
@@ -2,7 +2,19 @@ package com.nnk.springboot.repositories;
 
 import com.nnk.springboot.domain.BidList;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
+@Repository
 public interface BidListRepository extends JpaRepository<BidList, Integer> {
 
+    List<BidList> findAll();
+
+    BidList save(BidList bid);
+
+    void delete(BidList bid);
+
+    Optional<BidList> findById(Integer id);
 }
