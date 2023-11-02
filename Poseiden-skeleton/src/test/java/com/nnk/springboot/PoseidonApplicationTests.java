be7542e3f86eea69b13ef04d1ecb5b4f113dbb1b
Mikael Heyraud <mikaelhey@hotmail.fr>
2023-11-02 20:28:11 +0100

    MHE - moved tests

---
+++ Poseiden-skeleton/src/test/java/com/nnk/springboot/PoseidonApplicationTests.java
@@ -0,0 +1,11 @@
+package com.nnk.springboot;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class PoseidonApplicationTests {
+    @Test
+    public void contextLoads() {
+    }
+}
