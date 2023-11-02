3a33fd16b5ae7a31d7e0670ae8e8864fd8575654
Mikael Heyraud <mikaelhey@hotmail.fr>
2023-11-02 20:29:58 +0100

    MHE - Did TODO: call service find all bids to show to the view

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/service/impl/BidListServiceImpl.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/service/impl/BidListServiceImpl.java
@@ -1,14 +1,24 @@
 package com.nnk.springboot.service.impl;
 
+import com.nnk.springboot.domain.BidList;
 import com.nnk.springboot.repositories.BidListRepository;
+import com.nnk.springboot.service.BidListService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class BidListServiceImpl {
+public class BidListServiceImpl implements BidListService {
 
     @Autowired
     private BidListRepository bidListRepository;
 
-
+    /**
+     *{@inheritDoc}
+     */
+    @Override
+    public List<BidList> findAllBid() {
+        return bidListRepository.findAll();
+    }
 }
