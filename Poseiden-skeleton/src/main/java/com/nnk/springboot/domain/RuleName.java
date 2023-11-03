946e070749696f5528e3ccd5ea299c3b2a7868b7
Mikael Heyraud <mikaelhey@hotmail.fr>
2023-11-03 19:15:23 +0100

    MHE - create constructor for all models

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/RuleName.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/RuleName.java
@@ -10,5 +10,7 @@ import java.sql.Timestamp;
 @Entity
 @Table(name = "rulename")
 public class RuleName {
+    public RuleName(String rule_name, String description, String json, String template, String sql, String sql_part) {
+    }
     // TODO: Map columns in data table RULENAME with corresponding java fields
 }
--- Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/Trade.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/Trade.java
@@ -8,5 +8,7 @@ import java.sql.Timestamp;
 @Entity
 @Table(name = "trade")
 public class Trade {
+    public Trade(String trade_account, String type) {
+    }
     // TODO: Map columns in data table TRADE with corresponding java fields
 }
