602db4e151bbe09ef6be1c16023dbfb24235c03d
Mikael Heyraud <mikaelhey@hotmail.fr>
2023-11-13 20:21:11 +0100

    MHE - RuleName fields and test

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/RuleName.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/RuleName.java
@@ -15,26 +15,29 @@ import java.sql.Timestamp;
 @Getter
 @Setter
 @Table(name = "rulename")
-@NoArgsConstructor
 public class RuleName {
 
     // TODO: Map columns in data table RULENAME with corresponding java fields
     @Id
-    @Column(name = "")
+    @Column(name = "id")
     private Integer id;
 
+    @Column(name = "name")
     private String name;
 
+    @Column(name = "description")
     private String description;
 
+    @Column(name = "json")
     private String json;
 
+    @Column(name = "template")
     private String template;
 
+    @Column(name = "sql_str")
     private String sqlStr;
 
+    @Column(name = "sql_part")
     private String sqlPart;
 
-    public RuleName(String rule_name, String description, String json, String template, String sql, String sql_part) {
-    }
 }
