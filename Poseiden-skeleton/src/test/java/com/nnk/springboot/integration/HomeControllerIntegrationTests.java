3ed709a436ef3bfbaea2e691981797d37da0326b
SimonArduin <125870016+SimonArduin@users.noreply.github.com>
2023-11-20 11:33:21 +0100

    added integration tests for homecontroller

---
+++ Poseiden-skeleton/src/test/java/com/nnk/springboot/integration/HomeControllerIntegrationTests.java
@@ -0,0 +1,64 @@
+package com.nnk.springboot.integration;
+
+import com.nnk.springboot.TestVariables;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class HomeControllerIntegrationTests extends TestVariables {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    public void setUpPerTest() {
+        initializeVariables();
+    }
+
+    @Test
+    public void contextLoads() {}
+
+    @Nested
+    public class homeTests
+    {
+        @Test
+        @WithMockUser(authorities = "USER")
+        public void homeTest () throws Exception {
+            mockMvc.perform(get("/"))
+                    .andExpect(status().is2xxSuccessful());
+        }
+        @Test
+        @WithMockUser
+        public void homeTestIfNotAuthorized () throws Exception {
+            mockMvc.perform(get("/"))
+                    .andExpect(status().isForbidden());
+        }
+    }
+
+    @Nested
+    public class adminHomeTests
+    {
+        @Test
+        @WithMockUser(authorities = "ADMIN")
+        public void adminHomeTest () throws Exception {
+            mockMvc.perform(get("/admin/home"))
+                    .andExpect(status().is3xxRedirection());
+        }
+        @Test
+        @WithMockUser(authorities = "USER")
+        public void adminHomeTestIfNotAuthorized () throws Exception {
+            mockMvc.perform(get("/admin/home"))
+                    .andExpect(status().isForbidden());
+        }
+    }
+}
