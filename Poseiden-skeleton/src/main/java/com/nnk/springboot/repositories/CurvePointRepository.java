0997feaf34dddc836ddf262abdcccfccd413c6df
Mikael Heyraud <mikaelhey@hotmail.fr>
2023-11-24 17:53:21 +0100

    MHE - Added CurvePoint repo services and Impl

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/repositories/CurvePointRepository.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/repositories/CurvePointRepository.java
@@ -1,9 +1,24 @@
 package com.nnk.springboot.repositories;
 
+import com.nnk.springboot.domain.BidList;
 import com.nnk.springboot.domain.CurvePoint;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
 
 
 public interface CurvePointRepository extends JpaRepository<CurvePoint, Integer> {
 
+    List<CurvePoint> findAll();
+
+    CurvePoint save(CurvePoint curve);
+
+    void delete(CurvePoint curve);
+
+    Optional<CurvePoint> findById(Integer id);
+
+    CurvePoint findByCurvePointId(Integer id);
+    
+    boolean checkIfIdExists(int id);
 }
