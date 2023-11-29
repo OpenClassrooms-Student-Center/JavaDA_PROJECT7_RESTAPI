3f325d916c32bc6c8256b9ae47da9b1f6389f8ed
SimonArduin <125870016+SimonArduin@users.noreply.github.com>
2023-11-29 18:06:03 +0100

    updated CurveController, added unit tests

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/CurveController.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/CurveController.java
@@ -1,6 +1,8 @@
 package com.nnk.springboot.controllers;
 
 import com.nnk.springboot.domain.CurvePoint;
+import com.nnk.springboot.services.CurvePointService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -14,41 +16,59 @@ import javax.validation.Valid;
 @Controller
 public class CurveController {
     // TODO: Inject Curve Point service
+    private final CurvePointService curvePointService;
+    
+    @Autowired
+    public CurveController(CurvePointService curvePointService) {
+        this.curvePointService = curvePointService;
+    }
 
     @RequestMapping("/curvePoint/list")
     public String home(Model model)
     {
         // TODO: find all Curve Point, add to model
+        model.addAttribute("curvePoints", curvePointService.findAll());
         return "curvePoint/list";
     }
 
     @GetMapping("/curvePoint/add")
-    public String addBidForm(CurvePoint bid) {
+    public String addCurvePointForm(CurvePoint curvePoint) {
         return "curvePoint/add";
     }
 
     @PostMapping("/curvePoint/validate")
     public String validate(@Valid CurvePoint curvePoint, BindingResult result, Model model) {
         // TODO: check data valid and save to db, after saving return Curve list
+        if(!result.hasErrors()) {
+            curvePointService.save(curvePoint);
+        }
         return "curvePoint/add";
     }
 
     @GetMapping("/curvePoint/update/{id}")
     public String showUpdateForm(@PathVariable("id") Integer id, Model model) {
         // TODO: get CurvePoint by Id and to model then show to the form
+        CurvePoint curvePoint = curvePointService.findById(id).orElseThrow((() -> new IllegalArgumentException("Invalid user Id:" + id)));
+        model.addAttribute("curvePoint", curvePoint);
         return "curvePoint/update";
     }
 
     @PostMapping("/curvePoint/update/{id}")
-    public String updateBid(@PathVariable("id") Integer id, @Valid CurvePoint curvePoint,
+    public String updateCurvePoint(@PathVariable("id") Integer id, @Valid CurvePoint curvePoint,
                              BindingResult result, Model model) {
         // TODO: check required fields, if valid call service to update Curve and return Curve list
+        if(!result.hasErrors()) {
+            curvePointService.findById(id).orElseThrow((() -> new IllegalArgumentException("Invalid user Id:" + id)));
+            curvePointService.save(curvePoint);
+        }
         return "redirect:/curvePoint/list";
     }
 
     @GetMapping("/curvePoint/delete/{id}")
-    public String deleteBid(@PathVariable("id") Integer id, Model model) {
+    public String deleteCurvePoint(@PathVariable("id") Integer id, Model model) {
         // TODO: Find Curve by Id and delete the Curve, return to Curve list
+        curvePointService.findById(id).orElseThrow((() -> new IllegalArgumentException("Invalid user Id:" + id)));
+        curvePointService.deleteById(id);
         return "redirect:/curvePoint/list";
     }
 }
---
+++ Poseiden-skeleton/src/test/java/com/nnk/springboot/unit/controllers/CurveControllerTests.java
@@ -0,0 +1,151 @@
+package com.nnk.springboot.unit.controllers;
+
+import com.nnk.springboot.TestVariables;
+import com.nnk.springboot.controllers.CurveController;
+import com.nnk.springboot.domain.CurvePoint;
+import com.nnk.springboot.services.CurvePointService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.validation.BindingResult;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest(classes = CurveController.class)
+public class CurveControllerTests extends TestVariables {
+
+    @Autowired
+    CurveController curveController;
+
+    @MockBean
+    private CurvePointService curvePointService;
+
+    @MockBean
+    private BindingResult bindingResult;
+
+    @BeforeEach
+    public void setUpPerTest() {
+        initializeVariables();
+        curvePoint.setId(1);
+
+        when(curvePointService.findAll()).thenReturn(curvePointList);
+        when(curvePointService.findById(any(Integer.class))).thenReturn(curvePointOptional);
+        when(curvePointService.findById(any(Integer.class))).thenReturn(curvePointOptional);
+        when(bindingResult.hasErrors()).thenReturn(false);
+    }
+
+    @Test
+    public void ContextLoads() {}
+
+    @Nested
+    public class HomeTests
+    {
+        @Test
+        public void homeTest () {
+            assertEquals("curvePoint/list", curveController.home(model));
+            verify(curvePointService, Mockito.times(1)).findAll();
+        }
+    }
+
+    @Nested
+    public class AddTests
+    {
+        @Test
+        public void addCurvePointFormTest () {
+            assertEquals("curvePoint/add", curveController.addCurvePointForm(curvePoint));
+        }
+        @Test
+        public void addCurvePointFormTestIfEmpty () {
+            assertEquals("curvePoint/add", curveController.addCurvePointForm(new CurvePoint()));
+        }
+        @Test
+        public void addCurvePointFormTestIfNull () {
+            assertEquals("curvePoint/add", curveController.addCurvePointForm(null));
+        }
+    }
+
+    @Nested
+    public class ValidateTests
+    {
+        @Test
+        public void validateTest () {
+            assertEquals("curvePoint/add", curveController.validate(curvePoint, bindingResult, model));
+            verify(curvePointService, Mockito.times(1)).save(any(CurvePoint.class));
+        }
+        @Test
+        public void validateTestIfInvalidCurvePoint () {
+            when(bindingResult.hasErrors()).thenReturn(true);
+            assertEquals("curvePoint/add", curveController.validate(curvePoint, bindingResult, model));
+            verify(curvePointService, Mockito.times(0)).save(any(CurvePoint.class));
+        }
+    }
+
+    @Nested
+    public class ShowUpdateTests
+    {
+        @Test
+        public void showUpdateFormTest () {
+            assertEquals("curvePoint/update", curveController.showUpdateForm(curvePoint.getId(), model));
+            verify(curvePointService, Mockito.times(1)).findById(any(Integer.class));
+        }
+        @Test
+        public void showUpdateFormTestIfCurvePointNotFound () {
+            when(curvePointService.findById(any(Integer.class))).thenReturn(Optional.empty());
+            assertThrows(IllegalArgumentException.class, () -> curveController.showUpdateForm(curvePoint.getId(), model));
+            verify(curvePointService, Mockito.times(1)).findById(any(Integer.class));
+        }
+    }
+
+    @Nested
+    public class UpdateCurvePointTests
+    {
+        @Test
+        public void updateCurvePointTest () {
+            assertEquals("redirect:/curvePoint/list", curveController.updateCurvePoint(curvePoint.getId(), curvePoint, bindingResult, model));
+            verify(curvePointService, Mockito.times(1)).findById(any(Integer.class));
+            verify(curvePointService, Mockito.times(1)).save(any(CurvePoint.class));
+        }
+        @Test
+        public void updateCurvePointTestIfInvalidCurvePoint () {
+            when(bindingResult.hasErrors()).thenReturn(true);
+            assertEquals("redirect:/curvePoint/list", curveController.updateCurvePoint(curvePoint.getId(), curvePoint, bindingResult, model));
+            verify(curvePointService, Mockito.times(0)).findById(any(Integer.class));
+            verify(curvePointService, Mockito.times(0)).save(any(CurvePoint.class));
+        }
+        @Test
+        public void updateCurvePointTestIfCurvePointNotInDB () {
+            when(curvePointService.findById(any(Integer.class))).thenReturn(Optional.empty());
+            assertThrows(IllegalArgumentException.class, () -> curveController.updateCurvePoint(curvePoint.getId(), curvePoint, bindingResult, model));
+            verify(curvePointService, Mockito.times(1)).findById(any(Integer.class));
+            verify(curvePointService, Mockito.times(0)).save(any(CurvePoint.class));
+        }
+    }
+
+    @Nested
+    public class DeleteCurvePointTests
+    {
+        @Test
+        public void deleteCurvePointTest () {
+            assertEquals("redirect:/curvePoint/list", curveController.deleteCurvePoint(curvePoint.getId(), model));
+            verify(curvePointService, Mockito.times(1)).findById(any(Integer.class));
+            verify(curvePointService, Mockito.times(1)).deleteById(any(Integer.class));
+        }
+        @Test
+        public void deleteCurvePointTestIfCurvePointNotInDB () {
+            when(curvePointService.findById(any(Integer.class))).thenReturn(Optional.empty());
+            assertThrows(IllegalArgumentException.class, () -> curveController.deleteCurvePoint(curvePoint.getId(), model));
+            verify(curvePointService, Mockito.times(1)).findById(any(Integer.class));
+            verify(curvePointService, Mockito.times(0)).deleteById(any(Integer.class));
+        }
+    }
+}
