4f9b0692a3869d436a1bacc651d574ffc5ca0907
SimonArduin <125870016+SimonArduin@users.noreply.github.com>
2023-12-05 18:11:39 +0100

    added TradeController and unit tests

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/TradeController.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/TradeController.java
@@ -1,6 +1,9 @@
 package com.nnk.springboot.controllers;
 
 import com.nnk.springboot.domain.Trade;
+import com.nnk.springboot.domain.Trade;
+import com.nnk.springboot.services.TradeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -14,28 +17,41 @@ import javax.validation.Valid;
 @Controller
 public class TradeController {
     // TODO: Inject Trade service
+    private final TradeService tradeService;
+
+    @Autowired
+    public TradeController(TradeService tradeService) {
+        this.tradeService = tradeService;
+    }
 
     @RequestMapping("/trade/list")
     public String home(Model model)
     {
         // TODO: find all Trade, add to model
+        model.addAttribute("trades", tradeService.findAll());
         return "trade/list";
     }
 
     @GetMapping("/trade/add")
-    public String addUser(Trade bid) {
+    public String addTradeForm(Trade bid) {
         return "trade/add";
     }
 
     @PostMapping("/trade/validate")
     public String validate(@Valid Trade trade, BindingResult result, Model model) {
         // TODO: check data valid and save to db, after saving return Trade list
+        if (!result.hasErrors()) {
+            tradeService.save(trade);
+            return "redirect:/trade/list";
+        }
         return "trade/add";
     }
 
     @GetMapping("/trade/update/{id}")
     public String showUpdateForm(@PathVariable("id") Integer id, Model model) {
         // TODO: get Trade by Id and to model then show to the form
+        Trade trade = tradeService.findById(id).orElseThrow((() -> new IllegalArgumentException("Invalid trade Id:" + id)));
+        model.addAttribute("trade", trade);
         return "trade/update";
     }
 
@@ -43,12 +59,18 @@ public class TradeController {
     public String updateTrade(@PathVariable("id") Integer id, @Valid Trade trade,
                              BindingResult result, Model model) {
         // TODO: check required fields, if valid call service to update Trade and return Trade list
+        if(!result.hasErrors()) {
+            tradeService.findById(id).orElseThrow((() -> new IllegalArgumentException("Invalid user Id:" + id)));
+            tradeService.save(trade);
+        }
         return "redirect:/trade/list";
     }
 
     @GetMapping("/trade/delete/{id}")
     public String deleteTrade(@PathVariable("id") Integer id, Model model) {
         // TODO: Find Trade by Id and delete the Trade, return to Trade list
+        tradeService.findById(id).orElseThrow((() -> new IllegalArgumentException("Invalid trade Id:" + id)));
+        tradeService.deleteById(id);
         return "redirect:/trade/list";
     }
 }
---
+++ Poseiden-skeleton/src/test/java/com/nnk/springboot/unit/controllers/TradeControllerTests.java
@@ -0,0 +1,151 @@
+package com.nnk.springboot.unit.controllers;
+
+import com.nnk.springboot.TestVariables;
+import com.nnk.springboot.controllers.TradeController;
+import com.nnk.springboot.domain.Trade;
+import com.nnk.springboot.services.TradeService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.validation.BindingResult;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest(classes = TradeController.class)
+public class TradeControllerTests extends TestVariables {
+
+    @Autowired
+    TradeController tradeController;
+
+    @MockBean
+    private TradeService tradeService;
+
+    @MockBean
+    private BindingResult bindingResult;
+
+    @BeforeEach
+    public void setUpPerTest() {
+        initializeVariables();
+        trade.setId(1);
+
+        when(tradeService.findAll()).thenReturn(tradeList);
+        when(tradeService.findById(any(Integer.class))).thenReturn(tradeOptional);
+        when(tradeService.findById(any(Integer.class))).thenReturn(tradeOptional);
+        when(bindingResult.hasErrors()).thenReturn(false);
+    }
+
+    @Test
+    public void ContextLoads() {}
+
+    @Nested
+    public class HomeTests
+    {
+        @Test
+        public void homeTest () {
+            assertEquals("trade/list", tradeController.home(model));
+            verify(tradeService, Mockito.times(1)).findAll();
+        }
+    }
+
+    @Nested
+    public class AddTests
+    {
+        @Test
+        public void addTradeFormTest () {
+            assertEquals("trade/add", tradeController.addTradeForm(trade));
+        }
+        @Test
+        public void addTradeFormTestIfEmpty () {
+            assertEquals("trade/add", tradeController.addTradeForm(new Trade()));
+        }
+        @Test
+        public void addTradeFormTestIfNull () {
+            assertEquals("trade/add", tradeController.addTradeForm(null));
+        }
+    }
+
+    @Nested
+    public class ValidateTests
+    {
+        @Test
+        public void validateTest () {
+            assertEquals("redirect:/trade/list", tradeController.validate(trade, bindingResult, model));
+            verify(tradeService, Mockito.times(1)).save(any(Trade.class));
+        }
+        @Test
+        public void validateTestIfInvalidTrade () {
+            when(bindingResult.hasErrors()).thenReturn(true);
+            assertEquals("trade/add", tradeController.validate(trade, bindingResult, model));
+            verify(tradeService, Mockito.times(0)).save(any(Trade.class));
+        }
+    }
+
+    @Nested
+    public class ShowUpdateTests
+    {
+        @Test
+        public void showUpdateFormTest () {
+            assertEquals("trade/update", tradeController.showUpdateForm(trade.getId(), model));
+            verify(tradeService, Mockito.times(1)).findById(any(Integer.class));
+        }
+        @Test
+        public void showUpdateFormTestIfTradeNotFound () {
+            when(tradeService.findById(any(Integer.class))).thenReturn(Optional.empty());
+            assertThrows(IllegalArgumentException.class, () -> tradeController.showUpdateForm(trade.getId(), model));
+            verify(tradeService, Mockito.times(1)).findById(any(Integer.class));
+        }
+    }
+
+    @Nested
+    public class UpdateTradeTests
+    {
+        @Test
+        public void updateTradeTest () {
+            assertEquals("redirect:/trade/list", tradeController.updateTrade(trade.getId(), trade, bindingResult, model));
+            verify(tradeService, Mockito.times(1)).findById(any(Integer.class));
+            verify(tradeService, Mockito.times(1)).save(any(Trade.class));
+        }
+        @Test
+        public void updateTradeTestIfInvalidTrade () {
+            when(bindingResult.hasErrors()).thenReturn(true);
+            assertEquals("redirect:/trade/list", tradeController.updateTrade(trade.getId(), trade, bindingResult, model));
+            verify(tradeService, Mockito.times(0)).findById(any(Integer.class));
+            verify(tradeService, Mockito.times(0)).save(any(Trade.class));
+        }
+        @Test
+        public void updateTradeTestIfTradeNotInDB () {
+            when(tradeService.findById(any(Integer.class))).thenReturn(Optional.empty());
+            assertThrows(IllegalArgumentException.class, () -> tradeController.updateTrade(trade.getId(), trade, bindingResult, model));
+            verify(tradeService, Mockito.times(1)).findById(any(Integer.class));
+            verify(tradeService, Mockito.times(0)).save(any(Trade.class));
+        }
+    }
+
+    @Nested
+    public class DeleteTradeTests
+    {
+        @Test
+        public void deleteTradeTest () {
+            assertEquals("redirect:/trade/list", tradeController.deleteTrade(trade.getId(), model));
+            verify(tradeService, Mockito.times(1)).findById(any(Integer.class));
+            verify(tradeService, Mockito.times(1)).deleteById(any(Integer.class));
+        }
+        @Test
+        public void deleteTradeTestIfTradeNotInDB () {
+            when(tradeService.findById(any(Integer.class))).thenReturn(Optional.empty());
+            assertThrows(IllegalArgumentException.class, () -> tradeController.deleteTrade(trade.getId(), model));
+            verify(tradeService, Mockito.times(1)).findById(any(Integer.class));
+            verify(tradeService, Mockito.times(0)).deleteById(any(Integer.class));
+        }
+    }
+}
