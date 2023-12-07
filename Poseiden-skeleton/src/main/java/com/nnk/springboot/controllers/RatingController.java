9ebdef3d6ff5010129cc6ebd854cc6e6df91004f
Mikael Heyraud <mikaelhey@hotmail.fr>
2023-12-07 20:38:10 +0100

    MHE - Done Rating Repository Service and Controller

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/RatingController.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/RatingController.java
@@ -1,6 +1,9 @@
 package com.nnk.springboot.controllers;
 
+import com.nnk.springboot.domain.CurvePoint;
 import com.nnk.springboot.domain.Rating;
+import com.nnk.springboot.services.RatingService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -11,14 +14,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import jakarta.validation.Valid;
 
+import java.util.List;
+
 @Controller
 public class RatingController {
-    // TODO: Inject Rating services
+    // TODO DONE: Inject Rating services
+    @Autowired
+    private RatingService ratingService;
 
     @RequestMapping("/rating/list")
     public String home(Model model)
     {
-        // TODO: find all Rating, add to model
+        // TODO DONE: find all Rating, add to model
+        List<Rating> rating = ratingService.findAllRating();
+        model.addAttribute("rating",rating);
         return "rating/list";
     }
 
@@ -29,26 +38,58 @@ public class RatingController {
 
     @PostMapping("/rating/validate")
     public String validate(@Valid Rating rating, BindingResult result, Model model) {
-        // TODO: check data valid and save to db, after saving return Rating list
-        return "rating/add";
+        // TODO DONE: check data valid and save to db, after saving return Rating list
+        if (result.hasErrors()) {
+            return "rating/add";
+        }
+        try {
+            ratingService.saveRating(rating);
+            return "redirect:/rating/list";
+        } catch (Exception e) {
+            return "rating/add";
+        }
     }
 
     @GetMapping("/rating/update/{id}")
     public String showUpdateForm(@PathVariable("id") Integer id, Model model) {
         // TODO: get Rating by Id and to model then show to the form
+        if (!ratingService.checkIfIdExists(id)) {
+            return "redirect:/rating/list";
+        }
+        Rating rating = ratingService.findByRatingId(id);
+        model.addAttribute("rating",rating);
+
         return "rating/update";
     }
 
     @PostMapping("/rating/update/{id}")
     public String updateRating(@PathVariable("id") Integer id, @Valid Rating rating,
                              BindingResult result, Model model) {
-        // TODO: check required fields, if valid call services to update Rating and return Rating list
-        return "redirect:/rating/list";
+        // TODO DONE: check required fields, if valid call services to update Rating and return Rating list
+        if (!ratingService.checkIfIdExists(id)) {
+            return "redirect:/rating/list";
+        }
+        if (result.hasErrors()) {
+            return "rating/add";
+        }
+        try {
+            ratingService.saveRating(rating);
+            return "redirect:/rating/list";
+        }catch (Exception e) {
+            return "rating/add";
+        }
     }
 
     @GetMapping("/rating/delete/{id}")
     public String deleteRating(@PathVariable("id") Integer id, Model model) {
-        // TODO: Find Rating by Id and delete the Rating, return to Rating list
+        // TODO DONE: Find Rating by Id and delete the Rating, return to Rating list
+        if (!ratingService.checkIfIdExists(id)) {
+            return "redirect:/rating/list";
+        }
+        Rating rating = ratingService.findByRatingId(id);
+        ratingService.deleteRating(rating);
+        model.addAttribute("rating", ratingService.findAllRating());
+
         return "redirect:/rating/list";
     }
 }
