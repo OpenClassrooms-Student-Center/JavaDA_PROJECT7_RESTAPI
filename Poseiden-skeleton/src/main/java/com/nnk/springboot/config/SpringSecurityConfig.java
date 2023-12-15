a1eda259c393c0ebac307aa8b10de40e2909eaa3
SimonArduin <125870016+SimonArduin@users.noreply.github.com>
2023-12-15 15:08:19 +0100

    cleaned up SpringSecurityConfig

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/config/SpringSecurityConfig.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/config/SpringSecurityConfig.java
@@ -1,27 +1,18 @@
 package com.nnk.springboot.config;
 
 import com.nnk.springboot.services.CustomUserDetailsService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-import javax.sql.DataSource;
-
 @Configuration
 @EnableWebSecurity
 public class SpringSecurityConfig {
-    /**
-     * the persistence layer, defined in application.properties
-     */
-    @Autowired
-    private DataSource dataSource;
 
     /**
      * @return a CustomUserDetailsService that connects SpringSecurity UserDetails objects with domain User objects
