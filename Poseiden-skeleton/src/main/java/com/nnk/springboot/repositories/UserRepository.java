169ef4fe86e3f5d81266397bddb77696a1e04508
Mikael Heyraud <mikaelhey@hotmail.fr>
2023-12-22 20:31:15 +0100

    MHE - User service et repo + clean up

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/repositories/UserRepository.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/repositories/UserRepository.java
@@ -1,5 +1,6 @@
 package com.nnk.springboot.repositories;
 
+import com.nnk.springboot.domain.Trade;
 import com.nnk.springboot.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
@@ -7,8 +8,18 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer>, JpaSpecificationExecutor<User> {
 
     User findByUsername(String username);
+
+    boolean existsByUsername(String username);
+
+    @Override
+    boolean existsById(Integer integer);
+
+    Optional<User> findById(Integer id);
+
 }
--- Poseiden-skeleton/src/main/java/com/nnk/springboot/services/TradeService.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/services/TradeService.java
@@ -5,13 +5,30 @@ import com.nnk.springboot.domain.Trade;
 import java.util.List;
 
 public interface TradeService {
+    /**
+     * @return
+     */
     List<Trade> findAllTrade();
 
+    /**
+     * @param trade
+     */
     void saveTrade(Trade trade);
 
+    /**
+     * @param id
+     * @return
+     */
     Trade findTradeById(Integer id);
 
+    /**
+     * @param id
+     * @return
+     */
     boolean checkIfIdExists(int id);
 
+    /**
+     * @param trade
+     */
     void deleteTrade(Trade trade);
 }
