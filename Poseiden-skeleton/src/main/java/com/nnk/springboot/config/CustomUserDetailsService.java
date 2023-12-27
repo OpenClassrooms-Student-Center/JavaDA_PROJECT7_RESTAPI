0a469796ff71659b13bba3b1bd9151dda9b9825d
Mikael Heyraud <mikaelhey@hotmail.fr>
2023-12-27 19:58:47 +0100

    MHE - New security configuration

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/config/CustomUserDetailsService.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/config/CustomUserDetailsService.java
@@ -2,6 +2,7 @@ package com.nnk.springboot.config;
 
 import com.nnk.springboot.domain.User;
 import com.nnk.springboot.repositories.UserRepository;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -9,6 +10,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,18 +21,27 @@ public class CustomUserDetailsService implements UserDetailsService {
     @Autowired
     private UserRepository userRepository;
 
+    @ModelAttribute("remoteUser")
+    public String remoteUser(final HttpServletRequest request) {
+        return request.getRemoteUser();
+    }
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByUsername(username);
 
-        return new org.springframework.security.core.userdetails.User(user.getUsername(),
-                user.getPassword(), getGrantedAuthorities(user.getRole()));
-    }
+        if (user == null) {
+            throw new UsernameNotFoundException("This User doesn't exist :" + username);
+        }
 
+        return new CustomUserDetails(user);
+       // return new org.springframework.security.core.userdetails.User(user.getUsername(),user.getPassword(), getGrantedAuthorities(user.getRole()));
+    }
+/*
     private List<GrantedAuthority> getGrantedAuthorities(String role) {
         List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
         authorities.add(new SimpleGrantedAuthority("ROLE_" + role));
         return authorities;
-    }
+    }*/
 
 }
