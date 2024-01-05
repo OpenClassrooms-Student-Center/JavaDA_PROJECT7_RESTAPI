1ad736d4a26b53d442ebee690c88fec6cc8a2b2e
Mikael Heyraud <mikaelhey@hotmail.fr>
2024-01-05 15:54:19 +0100

    MHE - Clean up security

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/config/CustomUserDetailsService.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/config/CustomUserDetailsService.java
@@ -2,29 +2,18 @@ package com.nnk.springboot.config;
 
 import com.nnk.springboot.domain.User;
 import com.nnk.springboot.repositories.UserRepository;
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.ModelAttribute;
 
-import java.util.ArrayList;
-import java.util.List;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
 
     @Autowired
     private UserRepository userRepository;
-
-    @ModelAttribute("remoteUser")
-    public String remoteUser(final HttpServletRequest request) {
-        return request.getRemoteUser();
-    }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -37,11 +26,5 @@ public class CustomUserDetailsService implements UserDetailsService {
         return new CustomUserDetails(user);
        // return new org.springframework.security.core.userdetails.User(user.getUsername(),user.getPassword(), getGrantedAuthorities(user.getRole()));
     }
-/*
-    private List<GrantedAuthority> getGrantedAuthorities(String role) {
-        List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
-        authorities.add(new SimpleGrantedAuthority("ROLE_" + role));
-        return authorities;
-    }*/
 
 }
